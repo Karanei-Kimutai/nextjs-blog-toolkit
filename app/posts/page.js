cd5b28e76cff052409a75e38af604df055734fa0
Karanei-Kimutai <karaneijunior@gmail.com>
2026-04-24 19:23:00 +0300

    first sprint

---
+++ app/posts/page.js
@@ -0,0 +1,8 @@
+export default function PostsPage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold mb-6">All Blog Posts</h1>
+      <p className="text-gray-600">This will show all our blog posts</p>
+    </div>
+  )
+}
